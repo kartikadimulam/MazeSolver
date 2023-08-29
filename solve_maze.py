e71531c6e833777e4c5239c854a4b4a9d909e85f
kartikadimulam <kartik.adi1@gmail.com>
2023-08-29 12:23:36 -0700

    Update solve_maze.py

--- solve_maze.py
+++ solve_maze.py
@@ -1,4 +1,4 @@
-
+'''
 maze = [
 ['+','+','+','+','G','+'],
 ['+',' ','+',' ',' ','+'],
@@ -7,81 +7,65 @@ maze = [
 ['+',' ',' ',' ',' ','+'],
 ['+','+','+','+','+','+'] ]
 
+'''
+
 from Stack import Stack
-s = Stack()
+
+
 def solveMaze(maze, xposition, yposition):
 
+    s = Stack()
+    s.push([xposition, yposition])
     goal = False
     stepcount = 1
     maze[xposition][yposition]= stepcount
     
     while goal == False:
-        position = maze[xposition][yposition]
-        s.push(xposition)
-        s.push(yposition)
+
+        xposition = s.peek()[0]
+        yposition = s.peek()[1]
         
         #check north
         if maze[xposition-1][yposition] == ' ':
             stepcount += 1
-            s.push(stepcount)
             maze[xposition-1][yposition] = stepcount
-            xposition -= 1
+            s.push([xposition-1, yposition])
             
         #check west
         elif maze[xposition][yposition-1]==' ':
             stepcount += 1
-            s.push(stepcount)
             maze[xposition][yposition-1] = stepcount
-            yposition -= 1
+            s.push([xposition, yposition-1])
 
         #check south
         elif maze[xposition+1][yposition] == ' ':
             stepcount += 1
-            s.push(stepcount)
             maze[xposition+1][yposition] = stepcount
-            xposition += 1
+            s.push([xposition+1, yposition])
             
         #check east
         elif maze[xposition][yposition+1] == ' ':
             stepcount += 1
-            s.push(stepcount)
             maze[xposition][yposition+1] = stepcount
-            yposition += 1
+            s.push([xposition, yposition+1])
 
             
-        #maybe ur near the goal
+        #possibly near the goal
         elif maze[xposition-1][yposition]=='G':
             goal = True
-            s.pop()
         elif maze[xposition+1][yposition]=='G':
             goal = True
-            s.pop()
         elif maze[xposition][yposition-1]=='G':
             goal = True
-            s.pop()
         elif maze[xposition][yposition+1]=='G':
             goal=True
+
+        else:
             s.pop()
 
-            
-        #lets go back
-        else:
-
-            if maze[xposition-1][yposition] == stepcount-1:
-                xposition -= 1
-                s.push(xposition)
-            elif maze[xposition+1][yposition] == stepcount-1:
-                xposition += 1
-                s.push(xposition)
-            elif maze[xposition][yposition-1]==stepcount-1:
-                yposition -= 1
-                s.push(yposition)
-            elif maze[xposition][yposition+1]==stepcount-1:
-                yposition += 1
-                s.push(yposition)
-            
-            
-            
+        if s.isEmpty():
+            return False
+                      
 
     return goal
 
